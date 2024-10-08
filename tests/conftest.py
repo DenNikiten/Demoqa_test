4dfe0afbb5dddbcf31286c1606ad73990fc2b633
DenNikiten <nikidenal@mail.ru>
2024-10-08 13:45:37 +0500

    added conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
-import os
+from datetime import datetime
+import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-
 from selenium.webdriver.chrome.service import Service
 
 
@@ -15,12 +15,16 @@ def driver():
     # driver = webdriver.Firefox(service=service, options=options)
     # driver.maximize_window()
     # yield driver
+    # attach = driver.get_screenshot_as_png()
+    # allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
     # driver.quit()
 
     service = Service()
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     yield driver
+    attach = driver.get_screenshot_as_png()
+    allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
     driver.quit()
