b9514ff2b05f439285456ad186aadcac4ab11588
DenNikiten <nikidenal@mail.ru>
2024-10-08 13:47:48 +0500

    added allure for base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
@@ -9,78 +10,96 @@ class BasePage:
         self.driver = driver
         self.url = url
 
+    @allure.step('Open a browser')
     def open(self):
         self.driver.get(self.url)
 
+    @allure.step('Find a visible element')
     def element_is_visible(self, locator, timeout=5):
         self.go_to_element(self.element_is_present(locator))
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
+    @allure.step('Find visible elements')
     def elements_are_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
+    @allure.step('Find a present element')
     def element_is_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
+    @allure.step('Find present elements')
     def elements_are_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
 
+    @allure.step('Find a clickable element')
     def element_is_clickable(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
+    @allure.step('Find invisibility of element')
     def element_is_not_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
 
-    """Method wait until text to be present in element"""
-
+    @allure.step(' Method wait until text to be present in element')
     def text_is_present_in_element(self, locator, text, timeout=10):
         return wait(self.driver, timeout).until(EC.text_to_be_present_in_element(locator, text))
 
+    @allure.step('Go to specified element')
     def go_to_element(self, element):
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
+    @allure.step('Move cursor to element')
     def action_move_to_element(self, element):
         action = ActionChains(self.driver)
         action.move_to_element(element)
         action.perform()
 
+    @allure.step('Clear field')
     def clear_field(self, locator):
         self.element_is_visible(locator).clear()
 
+    @allure.step('Double click')
     def action_double_click(self, element):
         action = ActionChains(self.driver)
         action.double_click(element)
         action.perform()
 
+    @allure.step('Right click')
     def action_right_click(self, element):
         action = ActionChains(self.driver)
         action.context_click(element)
         action.perform()
 
+    @allure.step('Drag and drop by offset')
     def action_drag_and_drop_by_offset(self, element, x_coords, y_coords):
         action = ActionChains(self.driver)
         action.drag_and_drop_by_offset(element, x_coords, y_coords)
         action.perform()
 
+    @allure.step('Drag and drop element to element')
     def action_drag_and_drop_to_element(self, what, where):
         action = ActionChains(self.driver)
         action.drag_and_drop(what, where)
         action.perform()
 
+    @allure.step('Switch to new tab')
     def switch_to_new_tab(self, num_window):
         self.driver.switch_to.window(self.driver.window_handles[num_window])
 
+    @allure.step('Switch to alert')
     def switch_to_alert(self, locator):
         self.element_is_visible(locator).click()
         return self.driver.switch_to.alert
 
+    @allure.step('Switch to frame')
     def switch_to_frame(self, locator):
         return self.driver.switch_to.frame(locator)
 
+    @allure.step('Remove footer')
     def remove_footer(self):
         self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
         self.driver.execute_script("document.getElementById('close-fixedban').remove();")
 
+    @allure.step('select date by text')
     def set_date_by_text(self, element, value):
         select = Select(self.element_is_present(element))
         select.select_by_visible_text(value)
